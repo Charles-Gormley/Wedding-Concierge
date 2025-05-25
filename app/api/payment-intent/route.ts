80add22eb13f4f30337c3918b2ad6640aefa22cd
Charles E. Gormley <76138796+Charles-Gormley@users.noreply.github.com>
2025-05-25 01:18:49 -0400

    adding in customer id parameter

--- app/api/payment-intent/route.ts
+++ app/api/payment-intent/route.ts
@@ -27,6 +27,7 @@ export async function POST(req: Request) {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: 7_999, // TODO: Grab this dynamically from the priceId.
       currency: "usd",
+      customer: userId,
       metadata: { 
         "priceId": priceId,
         "userId": userId,
